f5d34946b0ce63642e239387d36a6a900e628bc5
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-02 15:23:48 +0100

    Notes on interface

--- InterfaceConcreteClass/OctagonClass.cs
+++ InterfaceConcreteClass/OctagonClass.cs
@@ -8,6 +8,10 @@ namespace InterfaceConcreteClass
 {
     public class OctagonClass : IPolygon
     {
+        // when implementing an interface you implement all its abstract method and propertie
+        // use public access modifiers on the imlemented methods and properties
+        //you can also explicitly implement an interface methods by using this syntax <Interface Name>.<Method Name>
+        // when explicitly implementing interface method do not make use of the public modifier
         public int SidesOfPolygon { get; set; }
         public int LengthOfPolygon { get; set; }
         public OctagonClass(int side, int length)
@@ -19,7 +23,7 @@ namespace InterfaceConcreteClass
         {
             return 8 * LengthOfPolygon ;
         }
-        public double GetArea()
+        public double IPolygon.GetArea()
         {
             return  2 * (1 + Math.Sqrt(2)) *( LengthOfPolygon * LengthOfPolygon);
         }
